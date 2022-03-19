ec983864e2051969401f886abb493ce691d84f26
RinatUpakov <upakovrinat1992@gmail.com>
2022-03-19 16:29:41 +0300

    Исправил скрипты в Gitbash.txt и newscript.sh

--- newscript.sh
+++ newscript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir test_dir
 cd test_dir
 mkdir dir1 dir2 dir3
 cd dir2
@@ -6,6 +7,8 @@ touch test1.txt test2.txt test3.txt test1.json test2.json
 mkdir dir4 dir5 dir6
 ls -la
 cd ..
-mv dir1/test1.txt dir2/test1.txtmv dir1/test1.txt dir2/test1.txt
+mv dir2/test1.txt dir2/test2.txt dir1
 echo "Done"
+
+
 
